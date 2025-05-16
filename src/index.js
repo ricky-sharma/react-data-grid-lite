87629a34508a37c0bf01167586bec8515d9be79f
Vinay Sharma <sharma.ricky@gmail.com>
2025-05-16 17:47:04 +1000

    updated code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './css/global.css';
 import './css/react-data-grid-lite.css';
 
-export { default as ReactDataGridLite } from './react-data-grid-lite';
+//export { default as ReactDataGridLite } from './react-data-grid-lite';
+import ReactDataGridLite from "./react-data-grid-lite";
+export default ReactDataGridLite;
