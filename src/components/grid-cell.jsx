e55de876109f5ba7a52e5ee8f08a4884f85cfbe5
Vinay Sharma <sharma.ricky@gmail.com>
2025-07-18 12:35:23 +1000

    refactor(GridCell): move <td> cell rendering logic to grid-cell.jsx and update related tests

---
+++ src/components/grid-cell.jsx
@@ -0,0 +1,135 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
+import React, { memo } from 'react';
+import { isNull } from '../helpers/common';
+import { useGridConfig } from '../hooks/use-grid-config';
+import { resolveColumnItems, resolveColumnType } from '../utils/component-utils';
+import EditableCellFields from './grid-edit/editable-cell-fields';
+
+const GridCell = memo(({
+    keyProp,
+    col,
+    isMobile,
+    computedColumnWidthsRef,
+    lastFixedIndex,
+    rowIndex,
+    baseRowIndex,
+    baseRow,
+    formattedRow,
+    onCellEdit,
+    onKeyDown,
+    onTouchStart,
+    commitChanges,
+    onCellChange,
+    revertChanges,
+    cellChangedFocusRef,
+    clickTimerRef,
+    didDoubleClickRef
+}) => {
+    const { state = {} } = useGridConfig() ?? {};
+    const {
+        columns,
+        enableCellEdit,
+        enableColumnResize,
+        editingCell
+    } = state;
+    const colWidth = computedColumnWidthsRef?.current?.find(i =>
+        i?.name === col?.name)?.width ?? 0;
+    const classNames = col?.class || '';
+    const colResizable = typeof col?.resizable === "boolean"
+        ? col?.resizable : enableColumnResize;
+    const editable = typeof col?.editable === "boolean"
+        ? col?.editable : enableCellEdit;
+    const columnValue = formattedRow[col?.name?.toLowerCase()];
+    const editableColumns = (col.concatColumns?.columns ?? [col?.name])
+        .map((colName, index) => {
+            const columnDef = columns?.find(c => c.name === colName);
+            const concatType = col.concatColumns?.editor?.[index];
+            const baseType = columnDef?.editor;
+            return {
+                colName,
+                type: resolveColumnType(concatType, baseType),
+                values: resolveColumnItems(concatType, baseType)
+            };
+        });
+    const fixedMeta = col?.fixed === true && !isMobile;
+    const leftPosition = computedColumnWidthsRef?.current?.find(i => i?.name === col?.name)?.leftPosition ?? '';
+    return (
+        <td
+            key={keyProp}
+            className={classNames + (editable === true ? ' editable-cell' : '')}
+            tabIndex={editable === true ? 0 : undefined}
+            style={{
+                width: colWidth,
+                maxWidth: colResizable ? undefined : colWidth,
+                minWidth: colResizable ? undefined : colWidth,
+                left: fixedMeta ? leftPosition : '',
+                position: fixedMeta ? 'sticky' : '',
+                zIndex: fixedMeta ? 6 : '',
+                backgroundColor: 'inherit',
+                boxShadow: (lastFixedIndex === keyProp && fixedMeta
+                    ? '#e0e0e0 -0.5px 0 0 0 inset'
+                    : ''),
+                contain: 'layout paint',
+                cursor: editable === true ? 'pointer' : 'default',
+            }}
+            onBlur={() => (cellChangedFocusRef.current = null)}
+            onDoubleClick={() => {
+                if (clickTimerRef.current) clearTimeout(clickTimerRef.current);
+                didDoubleClickRef.current = true;
+                if (editable) onCellEdit(col.name, rowIndex, baseRowIndex);
+            }}
+            onMouseDown={(e) => {
+                if (e.target instanceof HTMLElement && e.target.tagName === 'A') {
+                    e.preventDefault();
+                }
+                if (!editable) e.preventDefault();
+            }}
+            onKeyDown={(e) =>
+                onKeyDown(e, {
+                    editable,
+                    editingCell,
+                    rowIndex,
+                    col,
+                    columns,
+                    onCellEdit,
+                    baseRowIndex
+                })
+            }
+            onTouchStart={onTouchStart(() => {
+                if (editable === true) onCellEdit(col.name, rowIndex, baseRowIndex);
+            })}
+            data-row-index={rowIndex}
+            data-col-name={col?.name}
+        >
+            {editable === true &&
+                editingCell?.rowIndex === rowIndex &&
+                editingCell?.columnName === col?.name ? (
+                <EditableCellFields
+                    baseRow={baseRow}
+                    columnValue={columnValue}
+                    commitChanges={commitChanges}
+                    editableColumns={editableColumns}
+                    onCellChange={onCellChange}
+                    revertChanges={revertChanges}
+                />
+            ) : !isNull(col?.render) && typeof col?.render === 'function' ? (
+                col.render(formattedRow, baseRow)
+            ) : (
+                <div
+                    style={{
+                        height: '100%',
+                        textAlign: 'left',
+                        padding: '10px 25px'
+                    }}
+                    className="mg--0 pd--0"
+                    title={columnValue?.toString()}
+                >
+                    {columnValue?.toString()}
+                </div>
+            )}
+        </td>
+    );
+});
+
+export default GridCell;
--- src/components/grid-rows.jsx
+++ src/components/grid-rows.jsx
@@ -16,9 +16,9 @@ import { useTableCellNavigation } from '../hooks/use-table-cell-navigation';
 import { useWindowWidth } from '../hooks/use-window-width';
 import DeleteIcon from '../icons/delete-icon';
 import EditIcon from '../icons/edit-icon';
-import { formatRowData, resolveColumnItems, resolveColumnType } from '../utils/component-utils';
+import { formatRowData } from '../utils/component-utils';
 import { hideLoader, showLoader } from '../utils/loading-utils';
-import EditableCellFields from './grid-edit/editable-cell-fields';
+import GridCell from './grid-cell';
 
 const GridRows = ({
     state,
@@ -83,10 +83,8 @@ const GridRows = ({
         deleteButtonEnabled,
         editButtonEvent,
         deleteButtonEvent,
-        enableColumnResize,
         gridID,
         actionColumnAlign,
-        enableCellEdit,
         editingCell,
         onCellUpdate,
         editingCellData,
@@ -120,108 +118,28 @@ const GridRows = ({
             const formattedRow = formatRowData(baseRow, columns);
             const cols = Object.values(columns).map((col, key) => {
                 if (col?.hidden === true) return null;
-                const columnValue = formattedRow[col?.name?.toLowerCase()];
-                const classNames = col?.class || '';
-                const colWidth = computedColumnWidthsRef?.current?.find(i =>
-                    i?.name === col?.name)?.width ?? 0;
-                const colResizable = typeof col?.resizable === "boolean"
-                    ? col?.resizable : enableColumnResize;
-                const editable = typeof col?.editable === "boolean"
-                    ? col?.editable : enableCellEdit;
-                const editableColumns = (col.concatColumns?.columns ?? [col?.name])
-                    .map((colName, index) => {
-                        const columnDef = columns.find(c => c.name === colName);
-                        const concatType = col.concatColumns?.editor?.[index];
-                        const baseType = columnDef?.editor;
-                        return {
-                            colName,
-                            type: resolveColumnType(concatType, baseType),
-                            values: resolveColumnItems(concatType, baseType)
-                        };
-                    });
                 return (
-                    <td
+                    <GridCell
                         key={key}
-                        className={classNames + (editable === true
-                            ? " editable-cell" : "")}
-                        tabIndex={editable === true ? 0 : undefined}
-                        style={{
-                            width: colWidth,
-                            maxWidth: colResizable ? undefined : colWidth,
-                            minWidth: colResizable ? undefined : colWidth,
-                            left: (col?.fixed === true && !isMobile
-                                ? computedColumnWidthsRef?.current?.find(i =>
-                                    i?.name === col?.name)?.leftPosition ?? '' : ''),
-                            position: (col?.fixed === true && !isMobile ? 'sticky' : ''),
-                            zIndex: (col?.fixed === true && !isMobile ? 6 : ''),
-                            backgroundColor: 'inherit',
-                            boxShadow: (lastFixedIndex === key && !isMobile
-                                ? '#e0e0e0 -0.5px 0 0 0 inset' : ''),
-                            contain: 'layout paint',
-                            cursor: editable === true ? 'pointer' : 'default',
-                        }}
-                        onBlur={() => cellChangedFocusRef.current = null}
-                        onDoubleClick={() => {
-                            if (clickTimerRef.current) {
-                                clearTimeout(clickTimerRef.current);
-                            }
-                            didDoubleClickRef.current = true;
-                            if (editable) {
-                                onCellEdit(col.name, rowIndex, baseRowIndex);
-                            }
-                        }}
-                        onMouseDown={(e) => {
-                            if (e.target instanceof HTMLElement
-                                && e.target.tagName === 'A') {
-                                e.preventDefault();
-                            }
-                            if (!editable) e.preventDefault()
-                        }}
-                        onKeyDown={(e) => onKeyDown(e, {
-                            editable,
-                            editingCell,
-                            rowIndex,
-                            col,
-                            columns,
-                            onCellEdit,
-                            baseRowIndex
-                        })}
-                        onTouchStart={onTouchStart(() => {
-                            if (editable === true) onCellEdit(col.name, rowIndex, baseRowIndex);
-                        })}
-                        data-row-index={rowIndex}
-                        data-col-name={col?.name}
-                    >
-                        {editable === true &&
-                            editingCell?.rowIndex === rowIndex &&
-                            editingCell?.columnName === col?.name ? (
-                            <EditableCellFields
-                                baseRow={baseRow}
-                                columnValue={columnValue}
-                                commitChanges={commitChanges}
-                                editableColumns={editableColumns}
-                                onCellChange={onCellChange}
-                                revertChanges={revertChanges}
-                            />
-                        ) : (
-                            !isNull(col?.render)
-                                && typeof col?.render === 'function' ? (
-                                col.render(formattedRow, baseRow)
-                            ) : (
-                                <div
-                                    style={{
-                                        height: "100%",
-                                        textAlign: "left",
-                                        padding: "10px 25px"
-                                    }}
-                                    className="mg--0 pd--0"
-                                    title={columnValue?.toString()}
-                                >
-                                    {columnValue?.toString()}
-                                </div>
-                            )
-                        )}
-                    </td>
+                        keyProp={key}
+                        col={col}
+                        isMobile={isMobile}
+                        computedColumnWidthsRef={computedColumnWidthsRef}
+                        lastFixedIndex={lastFixedIndex}
+                        rowIndex={rowIndex}
+                        baseRowIndex={baseRowIndex}
+                        baseRow={baseRow}
+                        formattedRow={formattedRow}
+                        onCellEdit={onCellEdit}
+                        onKeyDown={onKeyDown}
+                        onTouchStart={onTouchStart}
+                        commitChanges={commitChanges}
+                        onCellChange={onCellChange}
+                        revertChanges={revertChanges}
+                        cellChangedFocusRef={cellChangedFocusRef}
+                        clickTimerRef={clickTimerRef}
+                        didDoubleClickRef={didDoubleClickRef}
+                    />
                 );
             });
             const isActionColumnLeft = actionColumnAlign === 'left';
